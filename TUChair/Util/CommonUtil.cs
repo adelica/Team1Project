483f94e155ae172c1962b6cb3f1d8a58ac10e788
현진 조 <whguswls312@naver.com>
2020-07-20 17:35:56 +0900

    공통그리드뷰 추가

---
+++ TUChair/Util/CommonUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TUChair.Util
+{
+    public class CommonUtil
+    {
+        public static void AddNewColumnToDataGridView(DataGridView dgv, string headerText, string dataPropertyName, bool visibility, int colWidth = 100, DataGridViewContentAlignment textAlign = DataGridViewContentAlignment.MiddleLeft)
+        {
+            DataGridViewTextBoxColumn gridCol = new DataGridViewTextBoxColumn();
+            gridCol.HeaderText = headerText;
+            gridCol.DataPropertyName = dataPropertyName;
+            gridCol.Width = colWidth;
+            gridCol.Visible = visibility;
+            gridCol.ValueType = typeof(string);
+            gridCol.ReadOnly = true;
+            gridCol.DefaultCellStyle.Alignment = textAlign;
+            dgv.Columns.Add(gridCol);
+        }
+
+        public static void InitSettingGridView(DataGridView dgv)
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+
+            dgv.ReadOnly = true;
+            dgv.AutoGenerateColumns = false;
+            dgv.AllowUserToAddRows = false;
+            dgv.AllowUserToResizeRows = false;
+            dgv.AllowUserToResizeColumns = false;
+            dgv.RowHeadersVisible = false;
+            dgv.MultiSelect = false;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+
+        }
+    }
+}
