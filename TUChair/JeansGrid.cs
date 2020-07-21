16c1496fea012a8fb8c56c8667daac07f0f258c9
현진 조 <whguswls312@naver.com>
2020-07-22 00:41:24 +0900

    GridView 기본 컨트롤 추가

---
+++ TUChair/JeansGrid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeanForm
+{
+    public partial class JeansGridView : DataGridView
+    {
+        public JeansGridView()
+        {
+            InitializeComponent();
+             this.DefaultCellStyle.Font = new Font("Calibri", 11, FontStyle.Bold);
+            
+            this.SelectionMode = DataGridViewSelectionMode.FullRowSelect; 
+            this.RowHeadersWidth = 30;
+            this.EnableHeadersVisualStyles = false;
+            this.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.ColumnHeadersHeight = 30;
+
+            this.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(52, 52, 52);
+            this.ColumnHeadersDefaultCellStyle.ForeColor = SystemColors.Info;
+            this.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(52, 52, 52);
+            this.ColumnHeadersDefaultCellStyle.SelectionForeColor = SystemColors.HighlightText;
+
+            this.ReadOnly = true;
+            this.DefaultCellStyle.BackColor = Color.White;
+            this.DefaultCellStyle.ForeColor = Color.FromArgb(52, 52, 52);
+            this.DefaultCellStyle.SelectionBackColor = Color.Gray;
+            this.DefaultCellStyle.SelectionForeColor = SystemColors.HighlightText;
+
+            //for (int i = 1; i < this.RowCount; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        this.Rows[i].DefaultCellStyle.BackColor = Color.Indigo;
+
+            //    }
+            //    else
+            //    {
+            //        this.Rows[i].DefaultCellStyle.BackColor = Color.White;
+            //    }
+            //}
+
+        }
+        
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+        
+    }
+}
