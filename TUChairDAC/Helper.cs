3b776398a4cb74e003a13d4ff7dfaf7bb9dd8ae5
최미령 <k48245568@gmail.com>
2020-07-21 21:25:42 +0900

    c

---
+++ TUChairDAC/Helper.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUChairDAC
+{
+   public static  class Helper
+    {
+        public static List<T> DataReaderMapToList<T>(IDataReader dr)
+        {
+            try
+            {
+                List<T> list = new List<T>();
+                T obj = default(T);
+                while (dr.Read())
+                {
+                    obj = Activator.CreateInstance<T>();
+                    foreach (PropertyInfo prop in obj.GetType().GetProperties())
+                    {
+                        if (!object.Equals(dr[prop.Name], DBNull.Value))
+                        {
+                            prop.SetValue(obj, dr[prop.Name], null);
+                        }
+                    }
+                    list.Add(obj);
+                }
+                return list;
+            }
+            catch (Exception err)
+            {
+                string msg = err.Message;
+
+                return null;
+            }
+        }
+        public static List<T> MeilingDataReaderMapToList<T>(IDataReader dr)
+        {
+            try
+            {
+                List<T> list = new List<T>();
+                T obj = default(T);
+                while (dr.Read())
+                {
+                    obj = Activator.CreateInstance<T>();
+                    DataTable dt = dr.GetSchemaTable();
+
+                    foreach (PropertyInfo prop in obj.GetType().GetProperties())
+                    {
+                        if (IsUseName(dt, prop.Name) && !object.Equals(dr[prop.Name], DBNull.Value))
+                        {
+                            prop.SetValue(obj, dr[prop.Name], null);
+                        }
+                    }
+                    list.Add(obj);
+                }
+                return list;
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 읽어 온 데이터에 있는 컬럼인지 확인 하는 메서드
+        /// </summary>
+        /// <param name="dt">스키마 데이터테이블</param>
+        /// <param name="name">VO프로퍼티 이름</param>
+        /// <returns></returns>
+        private static bool IsUseName(DataTable dt, string name)
+        {
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr["ColumnName"].ToString().ToUpper() == name.ToUpper())
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static List<T> ConvertDataTableToList<T>(DataTable table) where T : class, new()
+        {
+            try
+            {
+                List<T> list = new List<T>();
+                foreach (var row in table.AsEnumerable())
+                {
+                    T obj = new T();
+                    foreach (var prop in obj.GetType().GetProperties())
+                    {
+                        try
+                        {
+                            PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
+                            propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
+                        }
+                        catch (Exception err)
+                        {
+                            string msg = err.Message;
+                        }
+                    }
+                    list.Add(obj);
+                }
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- TUChairDAC/Properties/AssemblyInfo.cs
+++ TUChairDAC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("TUChairVO")]
+[assembly: AssemblyTitle("TUChairDAC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TUChairVO")]
+[assembly: AssemblyProduct("TUChairDAC")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("86693719-2496-448e-b793-d9dc958823a4")]
+[assembly: Guid("78d08b8b-d504-453c-a530-0f168698cb2f")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
