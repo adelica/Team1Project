3c73c7dcec1c92178346c26204225c4b5d0f4a28
최미령 <k48245568@gmail.com>
2020-07-21 20:27:10 +0900

    Meiling CommonDatagridView 체크박스 추가

--- TUChair/Util/CommonUtil.cs
+++ TUChair/Util/CommonUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace TUChair.Util
 {
     public class CommonUtil
     {
-        public static void AddNewColumnToDataGridView(DataGridView dgv, string headerText, string dataPropertyName, bool visibility, int colWidth = 100, DataGridViewContentAlignment textAlign = DataGridViewContentAlignment.MiddleLeft)
+        CheckBox headerChk;
+        DataGridView dgv1;
+        public  void AddNewColumnToDataGridView(DataGridView dgv, string headerText, string dataPropertyName, bool visibility, int colWidth = 100, DataGridViewContentAlignment textAlign = DataGridViewContentAlignment.MiddleLeft)
         {
+            dgv1 = dgv;
             DataGridViewTextBoxColumn gridCol = new DataGridViewTextBoxColumn();
             gridCol.HeaderText = headerText;
             gridCol.DataPropertyName = dataPropertyName;
@@ -20,6 +24,34 @@ namespace TUChair.Util
             gridCol.ReadOnly = true;
             gridCol.DefaultCellStyle.Alignment = textAlign;
             dgv.Columns.Add(gridCol);
+            DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+            chk.Width = 30;
+            chk.Name = "chk";
+            dgv1.Columns.Add(chk); //0 - checkbox
+
+            //데이터그리드뷰의 헤더에 위치할 체크박스
+            headerChk = new CheckBox();
+
+            Point headerCell = dgv1.GetCellDisplayRectangle(0, -1, true).Location;
+
+            headerChk.Location = new Point(headerCell.X + 8, headerCell.Y + 2);
+            headerChk.Size = new Size(18, 18);
+            headerChk.BackColor = Color.White;
+            headerChk.Click += HeaderChk_Clicked;
+            dgv1.Controls.Add(headerChk);
+        }
+
+        private void HeaderChk_Clicked(object sender, EventArgs e)
+        {
+
+            dgv1.EndEdit();
+
+            //데이터그리드뷰의 전체 행의 체크를 체크 or 언체크
+            foreach (DataGridViewRow row in dgv1.Rows)
+            {
+                DataGridViewCheckBoxCell chk = (DataGridViewCheckBoxCell)row.Cells["chk"];
+                chk.Value = headerChk.Checked;
+            }
         }
 
         public static void InitSettingGridView(DataGridView dgv)
