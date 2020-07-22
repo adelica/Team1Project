50c306ef200578fe768a2d4b35913bfd4093ab0d
vsIn <14dlstjd@daum.net>
2020-07-22 18:12:20 +0900

    In

--- TUChair/InForm/FactoryManage.cs
+++ TUChair/InForm/FactoryManage.cs
@@ -27,6 +27,15 @@ namespace TUChair.InForm
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설군", "Fact_Group", true);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설구분", "Fact_Class", true);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설코드", "Fact_Code", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설명", "Fact_Name", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설명", "Fact_Name", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "상위시설", "Fact_Parent", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설설명", "Fact_Information", true);
+ 
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "업체명", "Com_Name", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "사용유무", "Fact_UseOrNot", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "수정자", "Fact_Modifier", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "수정시간", "Fact_ModifyDate", true);
         }
       
     }
