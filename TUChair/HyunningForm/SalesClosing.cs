97ba57da8bdff839254951de0a8655a803d3d9c0
현진 조 <whguswls312@naver.com>
2020-07-28 23:56:26 +0900

    jean

---
+++ TUChair/HyunningForm/SalesClosing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TUChair.HyunningForm
+{
+    public partial class SalesClosing : TUChair.CommonForm.SearchCommomForm
+    {
+        public SalesClosing()
+        {
+            InitializeComponent();
+        }
+    }
+}
