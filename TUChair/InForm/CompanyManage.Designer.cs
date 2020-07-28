b2669a564d7a6c9f4244e9ee657f9bf73f264f67
vsIn <14dlstjd@daum.net>
2020-07-28 13:25:44 +0900

    수요계획 화면설계 및 dgv 재설정

--- TUChair/InForm/CompanyManage.Designer.cs
+++ TUChair/InForm/CompanyManage.Designer.cs
@@ -45,10 +45,18 @@
             this.label9 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
             this.dgvCompany = new JeanForm.JeansGridView();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCompany)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,28 +66,20 @@
             // 
             // label1
             // 
-            this.label1.Size = new System.Drawing.Size(93, 19);
+            this.label1.Size = new System.Drawing.Size(113, 23);
             this.label1.Text = "≡ 업체코드정의";
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.label9);
-            this.panel1.Controls.Add(this.txtFacG_Code);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label7);
             // 
             // txtFacG_Code
             // 
-            this.txtFacG_Code.Location = new System.Drawing.Point(120, 16);
+            this.txtFacG_Code.Location = new System.Drawing.Point(109, 3);
             this.txtFacG_Code.Name = "txtFacG_Code";
             this.txtFacG_Code.Size = new System.Drawing.Size(208, 21);
             this.txtFacG_Code.TabIndex = 0;
@@ -89,7 +89,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("굴림", 5F);
             this.label8.ForeColor = System.Drawing.Color.Black;
-            this.label8.Location = new System.Drawing.Point(16, 22);
+            this.label8.Location = new System.Drawing.Point(5, 9);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(10, 7);
             this.label8.TabIndex = 11;
@@ -99,7 +99,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(27, 20);
+            this.label7.Location = new System.Drawing.Point(16, 7);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(57, 12);
             this.label7.TabIndex = 10;
@@ -109,7 +109,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(27, 62);
+            this.label2.Location = new System.Drawing.Point(16, 7);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 12);
             this.label2.TabIndex = 10;
@@ -120,7 +120,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("굴림", 5F);
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(16, 64);
+            this.label3.Location = new System.Drawing.Point(5, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(10, 7);
             this.label3.TabIndex = 11;
@@ -128,7 +128,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 58);
+            this.textBox1.Location = new System.Drawing.Point(109, 3);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(208, 21);
             this.textBox1.TabIndex = 3;
@@ -137,7 +137,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(391, 20);
+            this.label4.Location = new System.Drawing.Point(18, 7);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 10;
@@ -148,7 +148,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("굴림", 5F);
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(380, 22);
+            this.label5.Location = new System.Drawing.Point(7, 9);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(10, 7);
             this.label5.TabIndex = 11;
@@ -156,7 +156,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(461, 16);
+            this.textBox2.Location = new System.Drawing.Point(88, 3);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(208, 21);
             this.textBox2.TabIndex = 1;
@@ -165,7 +165,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(722, 21);
+            this.label6.Location = new System.Drawing.Point(19, 8);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 12);
             this.label6.TabIndex = 10;
@@ -176,7 +176,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("굴림", 5F);
             this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(711, 23);
+            this.label9.Location = new System.Drawing.Point(8, 10);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(10, 7);
             this.label9.TabIndex = 11;
@@ -185,19 +185,60 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(802, 17);
+            this.comboBox1.Location = new System.Drawing.Point(99, 4);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(162, 20);
             this.comboBox1.TabIndex = 2;
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(979, 58);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "조회";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Location = new System.Drawing.Point(382, 15);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(299, 27);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.comboBox1);
+            this.panel5.Controls.Add(this.label6);
+            this.panel5.Controls.Add(this.label9);
+            this.panel5.Location = new System.Drawing.Point(717, 15);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(274, 27);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.textBox1);
+            this.panel6.Controls.Add(this.label2);
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Location = new System.Drawing.Point(11, 58);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(322, 27);
+            this.panel6.TabIndex = 3;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.txtFacG_Code);
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Controls.Add(this.label8);
+            this.panel7.Location = new System.Drawing.Point(11, 15);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(322, 27);
+            this.panel7.TabIndex = 0;
             // 
             // dgvCompany
             // 
@@ -221,10 +262,9 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvCompany.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvCompany.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvCompany.EnableHeadersVisualStyles = false;
+            this.dgvCompany.IsAllCheckColumnHeader = false;
             this.dgvCompany.Location = new System.Drawing.Point(0, 0);
             this.dgvCompany.Name = "dgvCompany";
-            this.dgvCompany.ReadOnly = true;
             this.dgvCompany.RowHeadersWidth = 30;
             dataGridViewCellStyle4.BackColor = System.Drawing.Color.Bisque;
             this.dgvCompany.RowsDefaultCellStyle = dataGridViewCellStyle4;
@@ -244,7 +284,14 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCompany)).EndInit();
             this.ResumeLayout(false);
 
@@ -265,6 +312,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel4;
         private JeanForm.JeansGridView dgvCompany;
     }
 }
---
+++ TUChair/InForm/DemandManage.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace TUChair
+{
+    partial class DemandManage
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.cboPlanID = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.cboCompany = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.txtItme_Code = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnProductionPlan = new System.Windows.Forms.Button();
+            this.dgvDemand = new JeanForm.JeansGridView();
+            this.dtpDate = new TUChair.InDTP();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDemand)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dgvDemand);
+            // 
+            // label1
+            // 
+            this.label1.Size = new System.Drawing.Size(83, 23);
+            this.label1.Text = "≡ 수요계획";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnProductionPlan);
+            this.panel2.Controls.SetChildIndex(this.label1, 0);
+            this.panel2.Controls.SetChildIndex(this.btnProductionPlan, 0);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel5);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.cboPlanID);
+            this.panel5.Controls.Add(this.label6);
+            this.panel5.Controls.Add(this.label9);
+            this.panel5.Location = new System.Drawing.Point(22, 12);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(274, 27);
+            this.panel5.TabIndex = 3;
+            // 
+            // cboPlanID
+            // 
+            this.cboPlanID.FormattingEnabled = true;
+            this.cboPlanID.Location = new System.Drawing.Point(99, 4);
+            this.cboPlanID.Name = "cboPlanID";
+            this.cboPlanID.Size = new System.Drawing.Size(162, 20);
+            this.cboPlanID.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(19, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "PlanID";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("굴림", 5F);
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(8, 10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 7);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "■";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.cboCompany);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(22, 59);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(274, 27);
+            this.panel4.TabIndex = 3;
+            // 
+            // cboCompany
+            // 
+            this.cboCompany.FormattingEnabled = true;
+            this.cboCompany.Location = new System.Drawing.Point(99, 4);
+            this.cboCompany.Name = "cboCompany";
+            this.cboCompany.Size = new System.Drawing.Size(162, 20);
+            this.cboCompany.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(19, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "업체";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 5F);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(8, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 7);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "■";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.dtpDate);
+            this.panel6.Controls.Add(this.label4);
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Location = new System.Drawing.Point(374, 12);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(345, 35);
+            this.panel6.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(19, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 5F);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(8, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 7);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "■";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.txtItme_Code);
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Controls.Add(this.label8);
+            this.panel7.Location = new System.Drawing.Point(725, 12);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(299, 27);
+            this.panel7.TabIndex = 4;
+            // 
+            // txtItme_Code
+            // 
+            this.txtItme_Code.Location = new System.Drawing.Point(88, 3);
+            this.txtItme_Code.Name = "txtItme_Code";
+            this.txtItme_Code.Size = new System.Drawing.Size(208, 21);
+            this.txtItme_Code.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(18, 7);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "품목";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("굴림", 5F);
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(7, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 7);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "■";
+            // 
+            // btnProductionPlan
+            // 
+            this.btnProductionPlan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProductionPlan.Location = new System.Drawing.Point(944, 7);
+            this.btnProductionPlan.Name = "btnProductionPlan";
+            this.btnProductionPlan.Size = new System.Drawing.Size(128, 23);
+            this.btnProductionPlan.TabIndex = 12;
+            this.btnProductionPlan.Text = "생산계획생성";
+            this.btnProductionPlan.UseVisualStyleBackColor = true;
+            // 
+            // dgvDemand
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Beige;
+            this.dgvDemand.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDemand.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvDemand.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvDemand.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvDemand.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDemand.IsAllCheckColumnHeader = false;
+            this.dgvDemand.Location = new System.Drawing.Point(0, 0);
+            this.dgvDemand.Name = "dgvDemand";
+            this.dgvDemand.RowHeadersWidth = 30;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Bisque;
+            this.dgvDemand.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvDemand.RowTemplate.Height = 23;
+            this.dgvDemand.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDemand.Size = new System.Drawing.Size(1090, 425);
+            this.dgvDemand.TabIndex = 0;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.End = new System.DateTime(2020, 7, 28, 9, 21, 20, 725);
+            this.dtpDate.Location = new System.Drawing.Point(81, 1);
+            this.dtpDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(245, 26);
+            this.dtpDate.Start = new System.DateTime(2020, 7, 28, 9, 21, 20, 725);
+            this.dtpDate.TabIndex = 12;
+            // 
+            // DemandManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.ClientSize = new System.Drawing.Size(1116, 602);
+            this.Name = "DemandManage";
+            this.Text = "수요계획";
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDemand)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ComboBox cboCompany;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.ComboBox cboPlanID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox txtItme_Code;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private InDTP dtpDate;
+        private JeanForm.JeansGridView dgvDemand;
+        private System.Windows.Forms.Button btnProductionPlan;
+    }
+}
