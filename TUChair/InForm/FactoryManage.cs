16bd46d5dda2745efc2ba5fb4a2323f23f7f8d8c
vsIn <14dlstjd@daum.net>
2020-07-29 13:26:04 +0900

    탭컨

--- TUChair/InForm/FactoryManage.cs
+++ TUChair/InForm/FactoryManage.cs
@@ -15,6 +15,7 @@ namespace TUChair.InForm
 {
     public partial class FactoryManage : Form
     {
+        List<FactoryVO> list = null;
         public FactoryManage()
         {
             InitializeComponent();
@@ -22,15 +23,16 @@ namespace TUChair.InForm
             CommonUtil.InitSettingGridView(dgvFactory);
 
 
-            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "no", "number", true,60);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "no", "Fact_No", true,60);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설군", "Fact_Group", true);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설구분", "Fact_Class", true);
-            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설코드", "Fact_Code", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설타입", "Fact_Type", true);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설코드", "Fact_Code", true,200);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설명", "Fact_Name", true);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설명", "Fact_Name", true);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "상위시설", "Fact_Parent", true);
-            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설설명", "Fact_Information", true);
-
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "시설설명", "Fact_Information", true,200);
+            CommonUtil.AddNewColumnToDataGridView(dgvFactory, "자재차감", "Fact_MatDeducation", true, 200);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "사용유무", "Fact_UseOrNot", true);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "수정자", "Fact_Modifier", true);
             CommonUtil.AddNewColumnToDataGridView(dgvFactory, "수정시간", "Fact_ModifyDate", true);
@@ -44,7 +46,7 @@ namespace TUChair.InForm
         private void DataLoad()
         {
             InService service = new InService();
-            List<FactoryVO> list = service.FactoryInfo();
+            list = service.FactoryInfo();
             dgvFactory.DataSource = list;
         }
     }
