8f77baaecc6f2895e99dd1ac32a09104ea0778f9
rptmddn <rptmddn@naver.com>
2020-07-29 09:36:19 +0900

    메인폼 수정중

---
+++ TUChair/TUChairMain2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TUChair.InForm;
+
+
+namespace TUChair
+{
+    public partial class TUChairMain2 : Form
+    {
+      
+        public TUChairMain2()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenorCreateForm<T>() where T : Form, new()
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form.GetType() == typeof(T))
+                {
+
+                    form.Activate();
+
+                    form.FormBorderStyle = FormBorderStyle.None;
+                    form.Dock = DockStyle.Fill;
+                    return;
+                }
+            }
+            T frm = new T();
+            frm.MdiParent = this;
+            frm.FormBorderStyle = FormBorderStyle.None;
+            frm.Dock = DockStyle.Fill;
+
+            frm.Show();
+        }
+
+      
+        private void TUChairMain_MdiChildActivate(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+                tabForms.Visible = false;
+            else
+            {
+                //this.ActiveMdiChild.WindowState = FormWindowState.Maximized;
+
+                if (this.ActiveMdiChild.Tag == null)
+                {
+                    //TabPage 생성자에 Text를 넘길때 공백을 추가로 넣어서 이미지와 붙지 않게
+                    TabPage tp = new TabPage(this.ActiveMdiChild.Text + "    ");
+                    tp.Tag = this.ActiveMdiChild;
+                    tp.Parent = tabForms;
+                    tabForms.SelectedTab = tp;
+
+                    this.ActiveMdiChild.Tag = tp;
+                    this.ActiveMdiChild.FormClosed += new FormClosedEventHandler(ActiveMdiChild_FormClosed);
+                }
+
+                if (!tabForms.Visible) tabForms.Visible = true;
+            }
+        }
+        private void ActiveMdiChild_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ((sender as Form).Tag as TabPage).Dispose();
+        }
+
+        private void tabForms_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (var i = 0; i < tabForms.TabPages.Count; i++)
+            {
+                var tabRect = tabForms.GetTabRect(i);
+                //tabRect.Inflate(-2, -2);
+                var closeImage = Properties.Resources.close_grey;
+                var imageRect = new Rectangle(
+                    (tabRect.Right - closeImage.Width),
+                    tabRect.Top + (tabRect.Height - closeImage.Height) / 2,
+                    closeImage.Width,
+                    closeImage.Height);
+                if (imageRect.Contains(e.Location))
+                {
+                    this.ActiveMdiChild.Close();
+                    //tabForms.TabPages.RemoveAt(i);                    
+                    break;
+                }
+            }
+        }
+        private void ShowMethod(List<author> authors, string programName)
+        { 
+            
+
+        }
+    }
+}
---
+++ TUChairVO/AuthorVO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUChairVO
+{
+    class AuthorVO
+    {
+        public string Program_ID { get; set; }
+        public string Program_Name { get; set; }
+        public int Program_order { get; set; }
+        public int Module_ID { get; set; }
+        public string Module_Name { get; set; }
+        public string  Method_Search { get; set; }
+        public string  Method_Insert { get; set; }
+        public string  Method_Save   { get; set; }
+        public string  Method_Delete { get; set; }
+        public string Method_Excel   { get; set; }
+    }    
+}
