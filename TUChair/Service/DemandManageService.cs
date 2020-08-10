b58e900ceb7e3fede75d350ccaf288d3b3dfa054
vsIn <14dlstjd@daum.net>
2020-08-10 18:20:29 +0900

    in

---
+++ TUChair/Service/DemandManageService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUChairDAC;
+
+namespace TUChair.Service
+{
+    class DemandManageService
+    {
+        internal DataTable GetDemandManage(string startDate, string endDate)
+        {
+            DemandManageDAC dac = new DemandManageDAC();
+            return dac.GetDemandManage(startDate, endDate);
+        }
+    }
+}
