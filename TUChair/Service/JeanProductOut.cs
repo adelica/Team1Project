fa4415a995acc8754f6688fb9d6026cf60b26ce8
현진 조 <whguswls312@naver.com>
2020-08-13 18:11:38 +0900

    ㅓㄷ무

---
+++ TUChair/Service/JeanProductOut.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUChairVO;
+using TUChairDAC;
+
+
+namespace TUChair.Service
+{
+    public class JeanProductOut
+    {
+        public List<ProductOutVO> POutBinding()
+        {
+            ProductOutDAC dac = new ProductOutDAC();
+            return dac.POutBinding();
+        }
+    }
+}
---
+++ TUChairVO/ProductOutVO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUChairVO
+{
+    public class ProductOutVO
+    {
+        public string So_PurchaseOrder { get; set; }
+        public string So_Duedate { get; set; }
+        public string So_WorkOrderID { get; set; }
+        public string Com_Code { get; set; }
+        public string Com_Name { get; set; }
+        public string d_Com_Code { get; set; }
+        public string d_Com_Name { get; set; }
+        public string Item_Code { get; set; }
+        public string d_Item_Code { get; set; }
+
+        public string Item_name { get; set; }
+        public int    So_Qty { get; set; }
+        public int    So_ShipQty { get; set; }
+        public int    Out_Unit { get; set; }
+
+    }
+}
