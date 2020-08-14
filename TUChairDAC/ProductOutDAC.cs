cc8942d0945b21f7b322f94a3b6a8f58bbcc2cc0
현진 조 <whguswls312@naver.com>
2020-08-14 18:17:04 +0900

    d

--- TUChairDAC/ProductOutDAC.cs
+++ TUChairDAC/ProductOutDAC.cs
@@ -17,12 +17,12 @@ namespace TUChairDAC
             {
                 SqlConnection conn = new SqlConnection(this.ConnectionString);
                 string sql = @"select So_PurchaseOrder,convert(nvarchar ,So_Duedate, 23) So_Duedate ,So_WorkOrderID,  s.Com_Code Com_Code, c.Com_Name Com_Name,
-		s.Com_Code d_Com_Code, c.Com_Name d_Com_Name, so.Item_Code Item_Code, i.Item_Code d_Item_Code,i.Item_name Item_name,
-  	   So_Qty, So_ShipQty, 0 as 'Out_Unit'
-from  SalesOrder so inner join SalesMaster s on so.Sales_ID=s.Sales_ID
-					inner join Company c on s.Com_Code=c.Com_Code
-					inner join Item i on so.Item_Code = i.Item_Code
-where So_Qty <>So_ShipQty";
+		                              s.Com_Code d_Com_Code, c.Com_Name d_Com_Name, so.Item_Code Item_Code, i.Item_Code d_Item_Code,i.Item_name Item_name,
+  	                                  So_Qty, So_ShipQty, 0 as 'Out_Unit'
+                                 from  SalesOrder so inner join SalesMaster s on so.Sales_ID=s.Sales_ID
+				                                     inner join Company c on s.Com_Code=c.Com_Code
+				                                     inner join Item i on so.Item_Code = i.Item_Code
+                                where So_Qty =So_ShipQty";
                 using (SqlCommand cmd = new SqlCommand(sql, conn))
                 {
                     conn.Open();
