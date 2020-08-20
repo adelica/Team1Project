a9936d8bdf8e332a29ad6920eca31004e361528f
현진 조 <whguswls312@naver.com>
2020-08-21 05:08:44 +0900

    LoginFrm

--- TUChair/Program.cs
+++ TUChair/Program.cs
@@ -23,7 +23,7 @@ namespace TUChair
             }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new POPForm());
+            Application.Run(new TUChairMain2());
         }
     }
 }
