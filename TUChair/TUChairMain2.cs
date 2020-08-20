4fa023ecb2e86101aaf34570d92cd360cd587a4f
최미령 <k48245568@gmail.com>
2020-08-20 15:09:37 +0900

    c

--- TUChair/TUChairMain2.cs
+++ TUChair/TUChairMain2.cs
@@ -675,7 +675,7 @@ namespace TUChair
         }
         private void toolStripButton4_Click(object sender, EventArgs e)
         {
-            OpenorCreateForm<UpdateWorkOrder>();
+
         }
 
         private void toolStripButton5_Click(object sender, EventArgs e)
